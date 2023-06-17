3689221807b6fc2bb2c0a99f34ae213ee5110560
Deepak <deepak403419@gmail.com>
2023-06-17 16:44:50 +0530

    m

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                © 2023 DEEPAK KUSHWAHA. All rights reserved. All trademarks are property of their respective owners in the India and other countries.
+                 VAT included in all prices where applicable. Using this site means u agreed the user terms and conditions.
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF/>
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
